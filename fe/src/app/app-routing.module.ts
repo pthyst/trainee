7d56f5c3ef7b6798e617eba4b3ffc34be27b5dab
Quân <37230937+pthyst@users.noreply.github.com>
2020-06-16 16:34:10 +0700

    Đang làm trang students

--- fe/src/app/app-routing.module.ts
+++ fe/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { StudentsComponent } from './pages/students/students.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'students', component: StudentsComponent}
 ];
 
 @NgModule({
